a988bb77799eeffc808d250db3179f052d3468d2
Luis <luisabad3210@gmail.com>
2021-03-16 17:34:03 -0400

    loops

---
+++ python_basics_3.py
@@ -0,0 +1,34 @@
+#DICTIONARIES
+
+weeksconversions = {
+    "Sun": "Sunday",
+    "Mon": "Monday",
+    "Tue": "Tuesday",
+    "Wed": "Wednsday",
+    "Thu": "Thursday",
+    "Fri": "Friday",
+    "Sat": "Saturday",
+}
+
+# print(weeksconversions["Fri"])
+
+
+#WHILE LOOP
+
+i = 1
+while i <= 10:
+    # print(i)
+    i = i + 1
+
+# print('done with loop')
+
+
+# FOR LOOPS
+
+# for letter in 'luis abad':
+#     print(letter)
+
+friends = ["luis", "joe", "ariel"]
+
+for friend in friends:
+    # print(friend)
