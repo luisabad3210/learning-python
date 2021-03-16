4a9b5bf5ac303b0fc2bb8ba0157b7c04b2a5e418
Luis <luisabad3210@gmail.com>
2021-03-16 14:17:04 -0400

    building basic calc

---
+++ python_basics.py
@@ -0,0 +1,49 @@
+# from math import *
+
+
+# VARIABLES AND DATA TYPES
+
+# character_name = "john"
+# character_age = "35"
+
+# print("his name is " + character_name + ",")
+# print("he is " + character_age + " years old")
+
+# character_name = "luis"
+# character_age = "27"
+
+# print("his name is " + character_name + ",")
+# print("he is " + character_age + " years old")
+
+
+
+# WORKING WITH STRINGS
+
+# phrase = "Giraffe Academy"
+# print("giraffe\nacademy")
+# print(len(phrase))
+# print(phrase + "is cool")
+
+
+# WORKING WITH NUMBERS
+
+# print(2)
+# print(3 - 4)
+
+# my_num = -5
+# print(str(my_num) + " my favorite number")
+# print(abs(my_num))
+# print(pow(3, 2))
+# print(max(3, 2))
+# print(min(3, 2))
+# print(round(3.5))
+
+
+# print(floor(3.2))
+# print(ceil(3.2))
+# print(sqrt(32))
+
+
+# name = input('enter your name: ')
+# age = input('enter your age: ')
+# print("hello " + name + ', you are ' + age)
