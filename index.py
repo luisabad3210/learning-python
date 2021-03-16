08d35243dc6e464b44fc04a65f271b3c3395efa1
Luis <luisabad3210@gmail.com>
2021-03-16 12:15:29 -0400

    variables and data types

--- index.py
+++ index.py
@@ -1,1 +1,12 @@
-print("Hello World")
+
+character_name = "john"
+character_age = "35"
+
+print("his name is " + character_name + ",")
+print("he is " + character_age + " years old")
+
+character_name = "luis"
+character_age = "27"
+
+print("his name is " + character_name + ",")
+print("he is " + character_age + " years old")
