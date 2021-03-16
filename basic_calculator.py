cc8300717635f832c2b8b02427bc080bca5e3445
Luis <luisabad3210@gmail.com>
2021-03-16 16:13:50 -0400

    basic calculator 2

--- basic_calculator.py
+++ basic_calculator.py
@@ -1,7 +1,25 @@
+# BASIC CALCULATOR
 
-num1 = input('enter a number: ')
-num2 = input('enter another number: ')
+# num1 = input('enter a number: ')
+# num2 = input('enter another number: ')
+# result = float(num1) + float(num2)
+# print(result)
 
-result = float(num1) + float(num2)
 
-print(result)
+# BASIC CALCULATOR 2
+
+num1 = float(input("enter 1st num"))
+op = input("action")
+num2 = float(input("enter 2nd num"))
+
+def calculate(num1, op, num2):
+    if (op == '+'):
+        return num1 + num2
+    elif (op == '-'):
+        return num1 - num2
+    elif (op == '/'):
+        return num1 / num2
+    else:
+        return num1 * num2
+
+print(calculate(num1, op, num2))
